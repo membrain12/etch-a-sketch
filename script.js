486d3b35795afb718c42db274caa993123945678
Paul Campbell <apcampbell12@gmail.com>
2022-07-31 08:39:34 -0500

    Add makeColumn function w/ color to test it worked

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 let gridSize = 16;
 
 function makeColumn() {
-    
+    let container = document.querySelector('.container');
+    let column = document.createElement('div');
+
+    for (let i = 0; i < gridSize; i++) {
+        let block = document.createElement('div');
+        block.classList.add('block');
+        block.style.width = `${300 / gridSize}px`;
+        block.style.height = `${300 / gridSize}px`;
+
+        column.appendChild(block);
+    }
+
+    container.appendChild(column);
 }
+
+makeColumn();
